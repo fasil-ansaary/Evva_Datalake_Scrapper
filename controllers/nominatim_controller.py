595360faeaaa577f3d64eeb156bdf1562d2aefb0
fasil-ansaary <mohamadfasilansaary@gmail.com>
2024-05-13 22:15:43 +0530

    added geocodertimeout limit

--- controllers/nominatim_controller.py
+++ controllers/nominatim_controller.py
@@ -1,7 +1,8 @@
 from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
 # import resources.constants as constants
 
-def get_coordinates(address, list):
+def get_coordinates(address, list, attempt=1, max_attempts=5):
     """
     This function uses mapbox api to get the locations coordinates in lattitude and in longitude.
 
@@ -15,13 +16,19 @@ def get_coordinates(address, list):
     Returns:
         list: list of scrapped details along with the coordinates.
     """
-    geocoder = Nominatim(user_agent="evva-datalake-scrapper")
-
-    location = geocoder.geocode(address)
-    if location:
-        latitude, longitude = location.latitude, location.longitude
-        list.extend([latitude, longitude])
-        return list
-    else:
-        list.extend(['Not Found', 'Not Found'])
-        return list
+    geocoder = Nominatim(user_agent="evva-datalake-scrapper")    
+    try:
+        location = geocoder.geocode(address)
+        if location:
+            latitude, longitude = location.latitude, location.longitude
+            list.extend([latitude, longitude])
+            return list
+        else:
+            list.extend(['Not Found', 'Not Found'])
+            return list
+    except GeocoderTimedOut:
+        if attempt <= max_attempts:
+            return get_coordinates(address, list, attempt=attempt+1)
+        raise
+    
+    

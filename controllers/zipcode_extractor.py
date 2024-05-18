fbca5f379461a6af939d343594645b099c579d4f
fasil-ansaary <mohamadfasilansaary@gmail.com>
2024-05-18 11:31:26 +0530

    changing constants

--- controllers/zipcode_extractor.py
+++ controllers/zipcode_extractor.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from resources.constants import state_id, zipcode
+import resources.constants as constants
 def zipcode_extractor():
     """
     This function takes a list of state names and returns the corresponding zips for the state.  
@@ -8,7 +8,7 @@ def zipcode_extractor():
     states_to_scrape = ["MI", "IL", "CA", "TX", "NY", "GA"]
     zipcodes = []
     for states in states_to_scrape:
-        filtered_values = df.loc[df[state_id] == states, zipcode].values
+        filtered_values = df.loc[df[constants.state_id] == states, constants.zipcode].values
         for value in filtered_values:
             zipcodes.append(value)
     return zipcodes

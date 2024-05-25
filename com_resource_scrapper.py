eaa911f7e17d10510d80b8d2676207eaee7704d6
fasil-ansaary <mohamadfasilansaary@gmail.com>
2024-05-26 00:29:53 +0530

    for "Alzheimer Association Chapters"

--- com_resource_scrapper.py
+++ com_resource_scrapper.py
@@ -44,9 +44,9 @@ class Community_resource_scrapper:
         self.options = Options()
         self.options.headless = True
         self.driver = webdriver.Chrome(options=self.options)     
-        scrapping_url = "https://www.communityresourcefinder.org/ProviderSearch/Search?ProfileDefinitionId=93&location="
-        care_type = "Alzheimer Education Programs"
-        file_name = "Alzheimer_Education_Programs" 
+        scrapping_url = "https://www.communityresourcefinder.org/ProviderSearch/Search?ProfileDefinitionId=84&location="
+        care_type = "Alzheimer Association Chapters"
+        file_name = "Alzheimer_Association_Chapters"
         with alive_bar(len(zipcodes)) as bar:              
             bar.title(f'Scrapping {care_type}:')
             for zip in zipcodes:

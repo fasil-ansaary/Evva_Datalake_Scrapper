79969da570c4cd2d86db12da5bc870d8f3da0715
fasil-ansaary <mohamadfasilansaary@gmail.com>
2024-06-14 00:09:42 +0530

    zipcode for ela

--- controllers/zipcode_extractor.py
+++ controllers/zipcode_extractor.py
@@ -4,7 +4,7 @@ def zipcode_extractor():
     This function takes a list of state names and returns the corresponding zips for the state.  
     """
     df = pd.read_csv('/home/evva-datalake-scrapper/Evva_Datalake_Scrapper/resources/uszips.csv')
-    states_to_scrape = ["FL"]
+    states_to_scrape = ["MI", "IL", "CA", "TX", "NY", "GA"]
     zipcodes = []
     for states in states_to_scrape:
         filtered_values = df.loc[df['state_id'] == states, 'zip'].values

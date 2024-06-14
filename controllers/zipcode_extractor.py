8b0fda27404129a8ca5576b4f91d7e59e0de8104
fasil-ansaary <mohamadfasilansaary@gmail.com>
2024-06-14 21:32:06 +0530

    for NY

--- controllers/zipcode_extractor.py
+++ controllers/zipcode_extractor.py
@@ -4,8 +4,8 @@ def zipcode_extractor(state_to_scrape):
     This function takes a list of state names and returns the corresponding zips for the state.  
     """
     # df = pd.read_csv('/Users/fasil/Desktop/scrapper script/Evva_Datalake_Scrapper/resources/uszips.csv')
-    # df = pd.read_csv('C:/Users/v2-scrapper/Desktop/Evva_Datalake_Scrapper/resources/uszips.csv')
-    df = pd.read_csv('/home/evva-datalake-scrapper/Evva_Datalake_Scrapper/resources/uszips.csv')
+    df = pd.read_csv('C:/Users/v2-scrapper/Desktop/Evva_Datalake_Scrapper/resources/uszips.csv')
+    # df = pd.read_csv('/home/evva-datalake-scrapper/Evva_Datalake_Scrapper/resources/uszips.csv')
     zipcodes = []
     filtered_values = df.loc[df['state_id'] == state_to_scrape, 'zip'].values
     for value in filtered_values:
